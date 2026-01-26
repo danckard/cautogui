fbd30d11148ec0104461978621c93ea04be18786
danckard <deckard.the.gamer@gmail.com>
2026-01-26 10:50:26 -0500

    modified:   cautogui/cautogui.py 	modified:   src/core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -2,7 +2,9 @@
 #include <Python.h>
 #include <windows.h>
 #include <vector>
-
+GdiplusStartupInput gdiplusStartupInput;
+ULONG_PTR gdiplusToken;
+GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 struct MonitorData {
     int x, y, w, h;
     bool is_primary;
