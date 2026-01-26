151effcd69ed599aec6a0ab1d8dff61f29b46e88
danckard <deckard.the.gamer@gmail.com>
2026-01-26 09:16:14 -0500

    Mantener extension de C++ original y añadir README

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, Extension, find_packages
-import os
 from pathlib import Path
+import os
+
+# Leer el README.md para que aparezca en PyPI
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
+# Tu extensión tal cual la pediste
 ext_module = Extension(
     'cautogui_core',
     sources=[os.path.join('src', 'core.cpp')],
@@ -11,33 +14,31 @@ ext_module = Extension(
     extra_compile_args=['/O2', '/std:c++17']
 )
 
-
 setup(
     name="cautogui",
-    # use_scm_version=True ya lo tienes para los tags
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     
-    # ESTO es lo que falta para PyPI:
+    # Metadatos para danckard en PyPI
+    author="danckard",
     description="High-performance GUI automation with C++ core",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    
-    author="danckard",
     url="https://github.com/danckard/cautogui",
     
+    project_urls={
+        "Source": "https://github.com/danckard/cautogui",
+        "Tracker": "https://github.com/danckard/cautogui/issues",
+    },
+
     packages=find_packages(),
-    ext_modules=[
-        Extension(
-            "cautogui.core",
-            sources=["cautogui/core.cpp"],
-            language='c++',
-        ),
-    ],
+    ext_modules=[ext_module], # Usamos tu objeto aquí
+    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ],
     python_requires='>=3.8',
+    zip_safe=False,
 )
